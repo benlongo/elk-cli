8f4ffa9a662413c15665d6797d2b81a4b5b354fb
Ben Longo <benlongo9807@gmail.com>
2022-08-08 19:21:02 -0400

    Make shared library work

--- src/main/java/com/valis/elk/nativeapi/Main.java
+++ src/main/java/com/valis/elk/nativeapi/Main.java
@@ -7,7 +7,11 @@ import org.eclipse.elk.graph.json.ElkGraphJson;
 import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CIntPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.UnsignedWord;
+
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     private static final IGraphLayoutEngine layoutEngine = new RecursiveGraphLayoutEngine();
@@ -15,18 +19,31 @@ public class Main {
     private static String Layout(String rawGraphJson) {
         var graph = ElkGraphJson.forGraph(rawGraphJson).toElk();
         layoutEngine.layout(graph, new NullElkProgressMonitor());
-        return ElkGraphJson.forGraph(graph)
-                .omitUnknownLayoutOptions(true)
-                .prettyPrint(true)
-                .toJson();
+        return ElkGraphJson.forGraph(graph).omitUnknownLayoutOptions(true).prettyPrint(true).toJson();
     }
+
+    static String result = null;
 
 
     @CEntryPoint(name = "Java_com_valis_elk_nativeapi_Layout")
-    public static CCharPointer NativeLayout(CCharPointer cRawGraphJson, IsolateThread isolateThread) {
-        String inputRawGraphJson = CTypeConversion.toJavaString(cRawGraphJson);
-        var resultRawGraphJson = Layout(inputRawGraphJson);
-        var resultPointerHolder = CTypeConversion.toCString(resultRawGraphJson);
-        return resultPointerHolder.get();
+    public static int NativeLayout(CCharPointer cRawGraphJson, CIntPointer sizeOut, @CEntryPoint.IsolateThreadContext IsolateThread isolateThread) {
+        try {
+            String inputRawGraphJson = CTypeConversion.toJavaString(cRawGraphJson);
+            result = Layout(inputRawGraphJson);
+            var numBytes = result.getBytes(StandardCharsets.UTF_8).length;
+            sizeOut.write(numBytes);
+            return 0;
+        } catch (Exception ignored) {
+            return -1;
+        }
+    }
+
+
+    @CEntryPoint(name = "Java_com_valis_elk_nativeapi_CopyResult")
+    public static UnsignedWord NativeCopyResult(CCharPointer buffer, UnsignedWord size, @CEntryPoint.IsolateThreadContext IsolateThread isolateThread) {
+        var bytesWritten = CTypeConversion.toCString(result, buffer, size);
+        buffer.write((int) bytesWritten.rawValue(), (byte) 0); // Null terminate it
+        result = null;
+        return bytesWritten;
     }
 }
